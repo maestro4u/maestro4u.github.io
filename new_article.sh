2ea1f3cc1709c54a1f7c1db1c46b80cf8a90c649
Hoon Kim <hoonkim@Hoonui-MacBookAir.local>
2025-03-09 18:32:15 +0900

    블로그 추가

---
+++ new_article.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# 사용 예: ./new_post.sh "포스트 제목"
+TITLE="$1"
+DATE=$(date +'%Y-%m-%d')
+FILENAME="_posts/articles/${DATE}-${TITLE// /-}.md"
+
+cat <<EOF > "$FILENAME"
+---
+layout: post
+title: "$TITLE"
+excerpt: []
+date: $(date +'%Y-%m-%d %H:%M:%S %z')
+tags: []
+categories: [articles]
+image:
+  feature: so-simple-sample-image-1.jpg
+  credit: WeGraphics
+  creditlink: http://wegraphics.net/downloads/free-ultimate-blurred-background-pack/
+comments: true
+share: true
+---
+EOF
+
+echo "Created new article: $FILENAME"
+
+# (추가) Cursor 앱으로 새로 생성된 파일 열기
+open -a "Cursor" "$FILENAME"
